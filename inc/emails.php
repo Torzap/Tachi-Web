bcec27154fb03c4fca2f40463e4c3148ff0f0042
Amir Torrez <amirtorrez@openmailbox.org>
2016-07-10 20:31:12 -0500

    Update emails.php

--- inc/emails.php
+++ inc/emails.php
@@ -1,29 +1,29 @@
 <?php
-	require('lib/class.phpmailer.php');
-	require('lib/class.smtp.php');
-	$mail = new PHPMailer ();
+require('lib/class.phpmailer.php');
+require('lib/class.smtp.php');
+$mail = new PHPMailer ();
 
-	$mail -> From = $wb['MAILUser'];
-	$mail -> FromName = $wb['MAILName'];
-	$mail -> AddAddress ("$Email");
-	$mail -> Subject = "$asunto"; 
-	$mail -> Body = '' .$Body. '';
-	$mail -> IsHTML (true);
-	
-	$mail->IsSMTP();
-	$mail->Host = $wb['MAILHost'];
-	$mail->Port = $wb['MAILPort'];
+$mail -> From = $wb['MAILUser'];
+$mail -> FromName = $wb['MAILName'];
+$mail -> AddAddress ("$Email");
+$mail -> Subject = "$asunto"; 
+$mail -> Body = '' .$Body. '';
+$mail -> IsHTML (true);
 
-	$mail->SMTPAuth = true;
+$mail->IsSMTP();
+$mail->Host = $wb['MAILHost'];
+$mail->Port = $wb['MAILPort'];
 
-	$mail->Username = $wb['MAILUser'];
-	$mail->Password = $wb['MAILPass'];
-	
-	#$mail->Send();
+$mail->SMTPAuth = true;
 
-	if(!$mail->Send()){
-		echo($errno);
-	} else {
-		echo($sucess);
-	};
+$mail->Username = $wb['MAILUser'];
+$mail->Password = $wb['MAILPass'];
+
+#$mail->Send();
+
+if(!$mail->Send()){
+	echo($errno);
+} else {
+	echo($sucess);
+};
 ?>
