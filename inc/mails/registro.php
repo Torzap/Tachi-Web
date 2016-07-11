d81f33e0827cf2716ed1fed17383d7628cf925a6
Amir Torrez <amirtorrez@openmailbox.org>
2016-07-10 20:33:30 -0500

    Create registro.php

---
+++ inc/mails/registro.php
@@ -0,0 +1,22 @@
+<?php
+$Body = 'Bienvenido a  '.$WebName.' los datos de su cuenta son los siguientes:</font><br><br>
+<font style="color:#36b222">Nombre de usuario: <strong>' .$UserID. '</strong> <br>
+Contraseña: <strong>' .$Password. '</strong><br>
+Secret Code: <strong>' .$TWCode. '</strong><br>
+
+<font size="2" style="color:#000000">Enhora buena usted se ha registrado para poder jugar en nuestro servidor de "Tantra Online".<br>
+Es nesesario que tome en cuenta los siguientes puntos antes de empezar a jugar.</font><br><br>
+<ol>
+<li>Recuerda no darle tu Nombre de usuario a nadie.<br>
+<li>Recuerda que al registrarte estas aceptando los términos y condiciones de uso.<br>
+<li>No te dejes engañar por gente que se haga pasar por algun GM de nuestro staff nosotros nunca pedimos contraseñas.<br>
+<li>Recomendamos revisar periódicamente nuestro foro para estar al tanto del servidor.<br>
+<li>Si es posible memorisa la información de tu cuenta y borra este mensaje lo mas pronto posible.<br><br>
+</ol></strong></font>
+<br>
+
+<font style="color:#000000">PD: No responda este mensaje, este mensaje ha sido enviado automáticamente por nuestro sistema tras su registro.<br>
+
+<font size="1" style="color:#000000">Este mensaje ha sido enviado automáticamente mediante los servicios de a pedido del remitente.<br> 
+Garantizamos que su dirección de correo no será compartida con terceros en ningún momento.</font>';
+?>
