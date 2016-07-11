c03918d09c98e253ff0f1a07e51ff4c8822c5070
Amir Torrez <amirtorrez@openmailbox.org>
2016-07-10 20:30:18 -0500

    Update and rename emails to emails.php

--- inc/emails.php
+++ inc/emails.php
@@ -3,21 +3,21 @@
 	require('lib/class.smtp.php');
 	$mail = new PHPMailer ();
 
-	$mail -> From = "$SMTPUser";
-	$mail -> FromName = "$SMTPName";
+	$mail -> From = $wb['MAILUser'];
+	$mail -> FromName = $wb['MAILName'];
 	$mail -> AddAddress ("$Email");
 	$mail -> Subject = "$asunto"; 
 	$mail -> Body = '' .$Body. '';
 	$mail -> IsHTML (true);
 	
 	$mail->IsSMTP();
-	$mail->Host = 'ssl://smtp.gmail.com';
-	$mail->Port = 465;
+	$mail->Host = $wb['MAILHost'];
+	$mail->Port = $wb['MAILPort'];
 
 	$mail->SMTPAuth = true;
 
-	$mail->Username = "$SMTPUser"; 
-	$mail->Password = "$SMTPPass";
+	$mail->Username = $wb['MAILUser'];
+	$mail->Password = $wb['MAILPass'];
 	
 	#$mail->Send();
 
